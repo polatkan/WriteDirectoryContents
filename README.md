9fee7ab6ade7149a852693ab599f585de7e34812
admin <polat.balcik@citysurf.com.tr>
2024-04-26 13:33:40 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # WriteDirectoryContents
+
+This program creates a TXT file that reflects all files and folders under the specified root directory in a nested structure. Indentation is used for nested folders.
+
+## How does it work:
+The SaveDirectoryTreeToFile procedure runs with the specified root directory and output filename.
+
+The WriteDirectoryContents procedure is called recursively. This procedure scans the files and subfolders for each folder, prints them via StreamWriter.
+Files and folders are indented so that the folder hierarchy is preserved.
+
+
+## Test
+After building the application;
+
+```bash
+WriteDirectoryContents.exe run the application by double-clicking
+```
+
+```bash
+Console:
+    cmd> WriteDirectoryContents.exe
+    cmd> WriteDirectoryContents.exe -c (It also writes the results to the console)
+```
+
+Writes the results to ```WriteDirectoryContents.txt``` file
+
+### Results (WriteDirectoryContents.txt)
+```sh
+[root]
+ ├── .gitignore
+ ├── LICENSE
+ ├── README.md
+ ├── [TEST1]
+ │    ├── test1.file
+ │    ├── [SUB-TEST1]
+ │    │    ├── subtest1_1.file
+ │    │    ├── subtest1_2.file
+ │    │    └── subtest1_3.file
+ │    ├── [SUB-TEST2]
+ │    │    └── subtest2_1.file
+ ├── [TEST2]
+ │    ├── test2_1.file
+ │    ├── test2_2.file
+ │    └── test2_3.file
+ └── [TEST3]
+```
+
+
---
+++ WriteDirectoryContents.dpr
@@ -0,0 +1,86 @@
+﻿program WriteDirectoryContents;
+
+{$APPTYPE CONSOLE}
+{$R *.res}
+
+uses
+  System.Classes,
+  System.SysUtils,
+  System.Types,
+  System.IOUtils;
+
+const
+  ROOT_TEXT = 'root';
+  FORMATTED_DIRECTORY = '[%s]';
+  INDENTATION_SPACE_SMALL = ' ';
+  INDENTATION_SPACE_BIG = '   ';
+  INDENTATION_LINE = '──' + INDENTATION_SPACE_SMALL;
+  INDENTATION_START = '├';
+  INDENTATION_FINISH = '└';
+  INDENTATION_CONTINUE = '│';
+  INDENTATION_TREE = INDENTATION_SPACE_SMALL + INDENTATION_START + INDENTATION_LINE;
+  INDENTATION_FILE: TStringDynArray = [INDENTATION_SPACE_SMALL + INDENTATION_START, INDENTATION_SPACE_SMALL + INDENTATION_FINISH];
+  INDENTATION_DIRECTORY: TStringDynArray = [INDENTATION_CONTINUE, INDENTATION_SPACE_SMALL];
+
+procedure DoWriteLine(const Value: string; const IsWriteConsole: Boolean; var OutputFile: TStreamWriter);
+begin
+  OutputFile.WriteLine(Value);
+  If IsWriteConsole then Writeln(Value);
+end;
+
+procedure DoWriteDirectoryContents(const Directory, Indent: string; const IsWriteConsole: Boolean; var OutputFile: TStreamWriter);
+var
+  i: Integer;
+  LastDir: Integer;
+  Files: TStringDynArray;
+  SubDirs: TStringDynArray;
+  SubDir: string;
+begin
+  Files := TDirectory.GetFiles(Directory);
+  for i := 0 to High(Files) - 1 do
+    DoWriteLine(Indent + INDENTATION_TREE + ExtractFileName(Files[i]), IsWriteConsole, OutputFile);
+  SubDirs := TDirectory.GetDirectories(Directory);
+  if Length(Files) > 0 then
+    DoWriteLine(Indent + INDENTATION_FILE[Ord(Length(SubDirs) = 0)] + INDENTATION_LINE + ExtractFileName(Files[High(Files)]),
+      IsWriteConsole, OutputFile);
+  for i := 0 to High(SubDirs) do
+  begin
+    SubDir := SubDirs[i];
+    LastDir := Ord(i = High(SubDirs));
+    DoWriteLine(Indent + INDENTATION_FILE[LastDir] + INDENTATION_LINE + Format(FORMATTED_DIRECTORY, [ExtractFileName(SubDir)]),
+      IsWriteConsole, OutputFile);
+    DoWriteDirectoryContents(SubDir, Indent + INDENTATION_SPACE_SMALL + INDENTATION_DIRECTORY[LastDir] + INDENTATION_SPACE_BIG,
+      IsWriteConsole, OutputFile);
+  end;
+end;
+
+procedure SaveDirectoryTreeToFile();
+var
+  OutputFileName: string;
+  IsWriteConsole: Boolean;
+  OutputFile: TStreamWriter;
+begin
+  OutputFileName := ChangeFileExt(ParamStr(0), '.txt');
+  OutputFile := TStreamWriter.Create(OutputFileName, False, TEncoding.UTF8);
+  try
+    IsWriteConsole := SameText(ParamStr(1), '-c');
+    DoWriteLine(Format(FORMATTED_DIRECTORY, [ROOT_TEXT]), IsWriteConsole, OutputFile);
+    DoWriteDirectoryContents(ExtractFileDir(ParamStr(0)), EmptyStr, IsWriteConsole, OutputFile);
+    WriteLn('Directory tree has been written to ', OutputFileName);
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+  OutputFile.Free;
+end;
+
+begin
+  try
+    SaveDirectoryTreeToFile();
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+  ReadLn;
+
+end.
